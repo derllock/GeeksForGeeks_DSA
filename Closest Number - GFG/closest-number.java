78e123ade5b5800a8272e37d334dd7dfe90d0145
Denis <denismaheshwari@gmail.com>
2023-07-18 19:04:22 +0530

    Update closest-number.java

--- Closest Number - GFG/closest-number.java
+++ Closest Number - GFG/closest-number.java
@@ -46,3 +46,18 @@ class Solution {
         return (int)res;     //double to int
     }
 }
+// // find the quotient
+//         int q = n / m;
+         
+//         // 1st possible closest number
+//         int n1 = m * q;
+         
+//         // 2nd possible closest number
+//         int n2 = (n * m) > 0 ? (m * (q + 1)) : (m * (q - 1));
+         
+//         // if true, then n1 is the required closest number
+//         if (Math.abs(n - n1) < Math.abs(n - n2))
+//             return n1;
+         
+//         // else n2 is the required closest number    
+//         return n2; 
